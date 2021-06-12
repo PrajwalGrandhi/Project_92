bd6b258e03ef90b3b1ab55c22c3e715471245f02
Prajwal Shreshta Grandhi <70463518+PrajwalGrandhi@users.noreply.github.com>
2021-06-12 22:30:06 +0530

    Add files via upload

---
+++ screens/SettingScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View ,TextInput,TouchableOpacity, Alert,Modal, ScrollView, KeyboardAvoidingView} from 'react-native';
+
+export default class SettingScreen extends React.Component{
+  render(){
+    return (
+        <View>
+<Text>settings</Text>
+        </View>
+    );
+  }
+}
---
+++ screens/ToDo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View ,TextInput,TouchableOpacity, Alert,Modal, ScrollView, KeyboardAvoidingView} from 'react-native';
+
+export default class ToDoScreen extends React.Component{
+  render(){
+    return (
+        <View>
+<Text>todo</Text>
+        </View>
+    );
+  }
+}
